8a819eba0aeb33827604db9ef53c397dbe1418d5
Rogier van der Geer <rogiervandergeer@godatadriven.com>
2024-02-22 20:45:27 +0100

    Fixes and updates of views

--- src/homeconnect_watcher/sql/1_active_events.sql
+++ src/homeconnect_watcher/sql/1_active_events.sql
@@ -5,10 +5,17 @@ WITH actv AS (
         *,
         CASE
             WHEN event in ('CONNECTED', 'DISCONNECTED') THEN FALSE
-            WHEN data->>'BSH.Common.Root.ActiveProgram' IS NOT NULL THEN TRUE
+            WHEN (
+                data->>'BSH.Common.Root.ActiveProgram' IS NOT NULL
+                AND (
+                    data->>'BSH.Common.Option.RemainingProgramTime' <> '0'
+                    OR NOT data ? 'BSH.Common.Option.RemainingProgramTime'
+                )
+            ) THEN TRUE
             WHEN data->>'BSH.Common.Status.OperationState' IS NULL THEN NULL
             WHEN data->>'BSH.Common.Status.OperationState' IN (
               'BSH.Common.EnumType.OperationState.Run',
+              'BSH.Common.EnumType.OperationState.Finished',
               'BSH.Common.EnumType.OperationState.Pause',
               'BSH.Common.EnumType.OperationState.Aborting',
               'BSH.Common.EnumType.OperationState.DelayedStart'
@@ -20,15 +27,14 @@ WITH actv AS (
 )
 
 SELECT
-    event_id,
     appliance_id,
     event,
     timestamp,
     data,
-    COALESCE(is_active, FIRST_VALUE(is_active) OVER (PARTITION BY appliance_id, grp ORDER BY timestamp, event_id), FALSE) AS is_active
+    COALESCE(is_active, FIRST_VALUE(is_active) OVER (PARTITION BY appliance_id, grp ORDER BY timestamp, event), FALSE) AS is_active
 FROM (
     SELECT
         *,
-        COUNT(is_active) OVER (PARTITION BY appliance_id ORDER BY timestamp, event_id) as grp
+        COUNT(is_active) OVER (PARTITION BY appliance_id ORDER BY timestamp, event) as grp
     FROM actv
 )
--- src/homeconnect_watcher/sql/3_session_ids.sql
+++ src/homeconnect_watcher/sql/3_session_ids.sql
@@ -5,12 +5,12 @@ WITH with_program AS (
     *,
     COALESCE(
         data->>'BSH.Common.Root.ActiveProgram',
-        FIRST_VALUE(data->>'BSH.Common.Root.ActiveProgram') OVER (PARTITION BY appliance_id, session_id ORDER BY timestamp, event_id)
+        FIRST_VALUE(data->>'BSH.Common.Root.ActiveProgram') OVER (PARTITION BY appliance_id, session_id, grp ORDER BY timestamp, event)
     ) AS active_program
   FROM (
      SELECT
        *,
-       COUNT(data->>'BSH.Common.Root.ActiveProgram') OVER (PARTITION BY appliance_id, session_id ORDER BY timestamp, event_id) as grp
+       COUNT(data->>'BSH.Common.Root.ActiveProgram') OVER (PARTITION BY appliance_id, session_id ORDER BY timestamp, event) as grp
      FROM active_sessions
   )
 ),
@@ -19,24 +19,23 @@ with_session_start AS (
   SELECT
     *,
     (
-        (is_active AND (LAG(is_active) OVER (PARTITION BY appliance_id ORDER BY timestamp, event_id) = False))
+        (is_active AND (LAG(is_active) OVER (PARTITION BY appliance_id ORDER BY timestamp, event) = False))
         OR
-        (active_program != (LAG(active_program) OVER (PARTITION BY appliance_id ORDER BY timestamp, event_id)))
+        (active_program != (LAG(active_program) OVER (PARTITION BY appliance_id ORDER BY timestamp, event)))
         OR
-        EXTRACT(EPOCH FROM (timestamp - LAG(timestamp) OVER (PARTITION BY appliance_id ORDER BY timestamp, event_id))) > 300
+        EXTRACT(EPOCH FROM (timestamp - LAG(timestamp) OVER (PARTITION BY appliance_id ORDER BY timestamp, event))) > 3600
     ) AS session_start
   FROM with_program
 ),
 
 with_session_id AS (
   SELECT
-    event_id,
     appliance_id,
     event,
     timestamp,
     data,
     is_active,
-    SUM(session_start::int) OVER (PARTITION BY appliance_id ORDER BY timestamp, event_id) as session_id
+    SUM(session_start::int) OVER (PARTITION BY appliance_id ORDER BY timestamp, event) as session_id
   FROM with_session_start
 )
 
